e84751558937cf5f85075740ae625868e35e649d
wglid <43250752+wglid@users.noreply.github.com>
2020-05-22 23:33:53 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ int main()
 	int i = 0;
 	Director dir;
 	Car CarBuilder;
+	Motobike MotoBuilder;
+	Quadrobike QuadroBuilder;
 	while(flag)
 	{
 		system("cls");
@@ -43,9 +45,23 @@ int main()
 			<< "[1] Добавить машину" << endl
 			<< "[2] Удалить машину" << endl
 			<< "[3] Редактировать машину" << endl
-			<< "[4] Вывести существующие собрки машин на экран" << endl 
+			<< "[4] Вывести существующие машины на экран" << endl 
 			<< "[5] Загрузка" << endl
 			<< "[6] Сохранение" << endl << endl
+			<< "\t" << "Мотоциклы" << endl
+			<< "[7] Добавить мотоцикл" << endl
+			<< "[8] Удалить мотоцикл" << endl
+			<< "[9] Редактировать мотоцикл" << endl
+			<< "[10] Вывести существующие мотоциклы на экран" << endl
+			<< "[11] Загрузка" << endl
+			<< "[12] Сохранение" << endl << endl
+			<< "\t" << "Квадроциклы" << endl
+			<< "[13] Добавить квадроцикл" << endl
+			<< "[14] Удалить квадроцикл" << endl
+			<< "[15] Редактировать квадроцикл" << endl
+			<< "[16] Вывести существующие квадроциклы на экран" << endl
+			<< "[17] Загрузка" << endl
+			<< "[18] Сохранение" << endl << endl
 			<< "[0] Выход" << endl;
 		cin >> button;
 		if (cin.fail()) {
@@ -53,6 +69,7 @@ int main()
 			cin.clear();
 			cin.ignore(10000, '\n');
 		}
+		system("CLS");
 		switch (button)
 		{
 		
@@ -60,13 +77,39 @@ int main()
 			Machinera.push_back(dir.createMachine(CarBuilder));
 			system("pause");
 			break;
-		
+		case 2:
+			if (!Machinera.size())
+			{
+				cout << "Элементов нет." << endl;
+			}
+			else
+			{
+				for (size_t i = 0; i < Machinera.size(); i++)
+				{
+					cout << "["<< i << "] " << Machinera[i]->arrayaddMark[0].mark << " " << Machinera[i]->arrayaddModel[0].model << endl;
+				}
+				cout << "Выш выбор: ";
+				cin >> idx;
+				Machinera.erase(Machinera.begin() + idx);
+				cout << "Элемент удален" << endl;
+			}
+			break;
 		case 3:
-			/*
-			Вывод типов элемета
-			cin >> x;
-			Machinera[x]->Edit();
-			*/
+			if (!Machinera.size())
+			{
+				cout << "Элементов нет." << endl;
+			}
+			else
+			{
+				for (size_t i = 0; i < Machinera.size(); i++)
+				{
+					cout << "[" << i << "] " << Machinera[i]->arrayaddMark[0].mark << " " << Machinera[i]->arrayaddModel[0].model << endl;
+				}
+				cout << "Выш выбор: ";
+				cin >> idx;
+				Machinera[idx]->Edit();
+			}
+			break;
 		case 4:
 			cout << "----------------------------------" << endl
 				<< "               МАШИНЫ               " << endl
@@ -85,16 +128,184 @@ int main()
 		case 5:
 			i = 0;
 			fin.open("Car.txt");
-			while (fin)
+			while (!fin.eof())
 			{
+				if (fin.peek() == -1)
+				{
+					break;
+				}
 				Machinera.push_back(new Machine);
 				Machinera[Machinera.size() - 1]->Load(fin);
 			}
 			fin.close();
+			if (!Machinera.size())
+				cout << "Файл пуст. Элементов нет!" << endl;
+			else
+				cout << "Из Файла было загружено " << Machinera.size() << " элементов." << endl;
 			break;
 		case 6:
 			i = 0;
 			fout.open("Car.txt");
+			for (size_t i = 0; i < Machinera.size(); i++)
+			{
+				Machinera[i]->Save(fout);
+			}
+			fout.close();
+			break;
+
+		case 7:
+			Machinera.push_back(dir.createMachine(MotoBuilder));
+			system("pause");
+			break;
+		case 8:
+			if (!Machinera.size())
+			{
+				cout << "Элементов нет." << endl;
+			}
+			else
+			{
+				for (size_t i = 0; i < Machinera.size(); i++)
+				{
+					cout << "[" << i << "] " << Machinera[i]->arrayaddMark[0].mark << " " << Machinera[i]->arrayaddModel[0].model << endl;
+				}
+				cout << "Выш выбор: ";
+				cin >> idx;
+				Machinera.erase(Machinera.begin() + idx);
+				cout << "Элемент удален" << endl;
+			}
+			break;
+		case 9:
+			if (!Machinera.size())
+			{
+				cout << "Элементов нет." << endl;
+			}
+			else
+			{
+				for (size_t i = 0; i < Machinera.size(); i++)
+				{
+					cout << "[" << i << "] " << Machinera[i]->arrayaddMark[0].mark << " " << Machinera[i]->arrayaddModel[0].model << endl;
+				}
+				cout << "Выш выбор: ";
+				cin >> idx;
+				Machinera[idx]->Edit();
+			}
+			break;
+		case 10:
+			cout << "----------------------------------" << endl
+				<< "               МОТОЦИКЛЫ           " << endl
+				<< "----------------------------------" << endl;
+			if (!Machinera.size())
+			{
+				cout << "Элементов нет." << endl;
+			}
+			else
+				for (size_t i = 0; i < Machinera.size(); i++)
+				{
+					Machinera[i]->info();
+				}
+			cout << endl;
+			break;
+		case 11:
+			i = 0;
+			fin.open("Motobike.txt");
+			while (!fin.eof())
+			{
+				if (fin.peek() == -1)
+				{
+					break;
+				}
+				Machinera.push_back(new Machine);
+				Machinera[Machinera.size() - 1]->Load(fin);
+			}
+			fin.close();
+			if (!Machinera.size())
+				cout << "Файл пуст. Элементов нет!" << endl;
+			else
+				cout << "Из Файла было загружено " << Machinera.size() << " элементов." << endl;
+			break;
+		case 12:
+			i = 0;
+			fout.open("Motobike.txt");
+			for (size_t i = 0; i < Machinera.size(); i++)
+			{
+				Machinera[i]->Save(fout);
+			}
+			fout.close();
+			break;
+
+		case 13:
+			Machinera.push_back(dir.createMachine(QuadroBuilder));
+			system("pause");
+			break;
+		case 14:
+			if (!Machinera.size())
+			{
+				cout << "Элементов нет." << endl;
+			}
+			else
+			{
+				for (size_t i = 0; i < Machinera.size(); i++)
+				{
+					cout << "[" << i << "] " << Machinera[i]->arrayaddMark[0].mark << " " << Machinera[i]->arrayaddModel[0].model << endl;
+				}
+				cout << "Выш выбор: ";
+				cin >> idx;
+				Machinera.erase(Machinera.begin() + idx);
+				cout << "Элемент удален" << endl;
+			}
+			break;
+		case 15:
+			if (!Machinera.size())
+			{
+				cout << "Элементов нет." << endl;
+			}
+			else
+			{
+				for (size_t i = 0; i < Machinera.size(); i++)
+				{
+					cout << "[" << i << "] " << Machinera[i]->arrayaddMark[0].mark << " " << Machinera[i]->arrayaddModel[0].model << endl;
+				}
+				cout << "Выш выбор: ";
+				cin >> idx;
+				Machinera[idx]->Edit();
+			}
+			break;
+		case 16:
+			cout << "----------------------------------" << endl
+				<< "               КВАДРОЦИКЛЫ         " << endl
+				<< "----------------------------------" << endl;
+			if (!Machinera.size())
+			{
+				cout << "Элементов нет." << endl;
+			}
+			else
+				for (size_t i = 0; i < Machinera.size(); i++)
+				{
+					Machinera[i]->info();
+				}
+			cout << endl;
+			break;
+		case 17:
+			i = 0;
+			fin.open("Quadrobike.txt");
+			while (!fin.eof())
+			{
+				if (fin.peek() == -1)
+				{
+					break;
+				}
+				Machinera.push_back(new Machine);
+				Machinera[Machinera.size() - 1]->Load(fin);
+			}
+			fin.close();
+			if (!Machinera.size())
+				cout << "Файл пуст. Элементов нет!" << endl;
+			else
+				cout << "Из Файла было загружено " << Machinera.size() << " элементов." << endl;
+			break;
+		case 18:
+			i = 0;
+			fout.open("Quadrobike.txt");
 			for (size_t i = 0; i < Machinera.size(); i++)
 			{
 				Machinera[i]->Save(fout);
@@ -114,12 +325,3 @@ int main()
 	return 0;
 }
 
-/*Data.Save();
-Data.Load();
-Data.Edit();
-Data.Save();
-//Data.Print(out);
-//Data.Load();
-Data.Delete();
-Data.Load();
-//Data.AddUp();*/
